c2993b116bda87ffe7dbe1bf6b28a496b94d9929
Hugo Rodriguez <hugo.rodriguezrivero@riedel.net>
2024-08-06 17:38:29 +0200

    lualine custom config

--- init.lua
+++ init.lua
@@ -13,3 +13,44 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving dow
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving down half-page" })
 vim.keymap.set("n", "G", "Gzz", { desc = "Center cursor after moving down half-page" })
 vim.cmd("colorscheme kanagawa")
+
+require("lualine").setup({
+    options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        },
+    },
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {},
+})
