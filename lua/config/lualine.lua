2aa028069d678cd049aa8468e5a15e5c48749a7f
Hugo Rodriguez <hugo.rodriguezrivero@riedel.net>
2024-08-22 12:47:39 +0200

    add macro recording to lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -20,7 +20,14 @@ require("lualine").setup({
     sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
+        lualine_c = {
+            {
+                require("noice").api.statusline.mode.get,
+                cond = require("noice").api.statusline.mode.has,
+                color = { fg = "#ff9e64" },
+            },
+            "filename",
+        },
         lualine_x = { "fileformat", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
