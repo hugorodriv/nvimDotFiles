d22abf36ed0039170c7b4cab64f62784ca79f71e
Hugo Rodriguez <hugo.rodriguezrivero@riedel.net>
2024-08-29 14:57:28 +0200

    disable logo

--- lua/plugins/customLogo.lua
+++ lua/plugins/customLogo.lua
@@ -3,17 +3,17 @@ return {
         "nvimdev/dashboard-nvim",
         event = "VimEnter",
         opts = function(_, opts)
-            local logo = [[
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                              ╱|、    
-                                                                                                                                                             (˚ˎ。7   
-                                                                                                                                                             |、˜〵   
-                                                                                                                                                             じしˍ,)ノ
-    ]]
-
-            logo = [[]]
+            --         local logo = [[
+            --
+            --
+            --                                                                                                                                                           ╱|、
+            --                                                                                                                                                          (˚ˎ。7
+            --                                                                                                                                                          |、˜〵
+            --                                                                                                                                                          じしˍ,)ノ
+            -- ]]
             -- logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+            local logo = [[]]
             logo = string.rep("\n", 13) .. logo .. "\n\n"
             opts.config.header = vim.split(logo, "\n")
             opts.theme = "doom"
